3128ee157a9fff967c31929d8a3e593ca62bf556
Aniket Singh <aniketsingh6990@gmail.com>
2023-01-06 19:17:54 +0530

    menu bar and event console page restructure

--- webview/src/pages/ExecutionPage/index.tsx
+++ webview/src/pages/ExecutionPage/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import ConfigArea from "../../components/ConfigArea";
-import ConsoleArea from "../../components/ConsoleArea";
 import ContractArea from "../../components/ContractArea";
 
 const Executioncontainer = styled.div`
-  width: 100%;
   overflow-y: scroll;
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-  grid-template-rows: 1fr 0.3fr;
-  padding: 20px;
-  row-gap: 20px;
+  grid-template-rows: repeat(1, 1fr);
 `;
 
 const Assessibilty = styled.div`
   height: auto;
   display: grid;
-  grid-template-columns: 1fr 0.1fr 1fr 0.2fr;
+  grid-template-columns: 1fr 0.1fr 1fr;
   grid-template-rows: 1fr;
   overflow-x: hidden;
 `;
@@ -38,7 +34,6 @@ const ExecutionPage = () => {
         <Divider></Divider>
         <ContractArea />
       </Assessibilty>
-      <ConsoleArea />
     </Executioncontainer>
   );
 };
